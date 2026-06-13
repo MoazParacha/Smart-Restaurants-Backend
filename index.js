314c290356b2350dc8ce60b88bbacc07b8ea7af2
MoazParacha <n1nflexacc@gmail.com>
2026-06-14 00:55:46 +0500

    fix port 8080

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const inventoryRoutes = require('./routes/inventoryRoutes');
 const tableRoutes = require('./routes/tableRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
